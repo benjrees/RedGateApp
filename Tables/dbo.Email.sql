899c81412b1b0cc54ac6804b40595636ba2314b8
benjrees <benjrees@gmail.com>
2013-02-11 13:43:35 +0000

    add lastname

--- Tables/dbo.Email.sql
+++ Tables/dbo.Email.sql
@@ -2,6 +2,7 @@ CREATE TABLE [dbo].[Email]
 (
 [id] [int] NULL,
 [email] [varchar] (50) COLLATE Latin1_General_CI_AS NULL,
-[firstname] [varchar] (50) COLLATE Latin1_General_CI_AS NULL
+[firstname] [varchar] (50) COLLATE Latin1_General_CI_AS NULL,
+[lastname] [varchar] (50) COLLATE Latin1_General_CI_AS NULL
 ) ON [PRIMARY]
 GO
