d3dbdd2c8d05347b0932a9c1b52391e4cef3c4fd
benjrees <benjrees@gmail.com>
2013-03-21 09:10:06 +0000

    Change ln

--- Tables/dbo.Email.sql
+++ Tables/dbo.Email.sql
@@ -3,7 +3,7 @@ CREATE TABLE [dbo].[Email]
 [id] [int] NULL,
 [email] [varchar] (max) COLLATE Latin1_General_CI_AS NULL,
 [firstname] [varchar] (max) COLLATE Latin1_General_CI_AS NULL,
-[lastname] [varchar] (50) COLLATE Latin1_General_CI_AS NULL,
+[lastname] [varchar] (max) COLLATE Latin1_General_CI_AS NULL,
 [address] [varchar] (50) COLLATE Latin1_General_CI_AS NULL
 ) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
 GO
