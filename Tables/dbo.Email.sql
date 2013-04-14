b9ac165655527a3a79d9edf5956eec6c502aa94e
benjrees <benjrees@gmail.com>
2013-04-14 20:39:55 +0100

    Add data

--- Tables/dbo.Email.sql
+++ Tables/dbo.Email.sql
@@ -1,9 +1,11 @@
 CREATE TABLE [dbo].[Email]
 (
-[id] [int] NULL,
+[id] [int] NOT NULL,
 [email] [varchar] (50) COLLATE Latin1_General_CI_AS NULL,
 [firstname] [varchar] (50) COLLATE Latin1_General_CI_AS NULL,
 [lastname] [varchar] (50) COLLATE Latin1_General_CI_AS NULL,
 [address] [varchar] (50) COLLATE Latin1_General_CI_AS NULL
 ) ON [PRIMARY]
+ALTER TABLE [dbo].[Email] ADD 
+CONSTRAINT [PK_Email] PRIMARY KEY CLUSTERED  ([id]) ON [PRIMARY]
 GO
