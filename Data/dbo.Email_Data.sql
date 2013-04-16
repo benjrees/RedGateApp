d2398824ca6eea9f52ac3e5c3c9e9f37f4eee840
benjrees <benjrees@gmail.com>
2013-04-16 06:20:57 +0100

    Change data for test

--- Data/dbo.Email_Data.sql
+++ Data/dbo.Email_Data.sql
@@ -1,4 +1,4 @@
 INSERT INTO [dbo].[Email] ([id], [email], [firstname], [lastname], [address]) VALUES (1, 'paul.mccartney@beatles.com', 'Paul', 'McCartney', NULL)
 INSERT INTO [dbo].[Email] ([id], [email], [firstname], [lastname], [address]) VALUES (2, 'john.lennon@beatles.com', 'John', 'Lennon', NULL)
 INSERT INTO [dbo].[Email] ([id], [email], [firstname], [lastname], [address]) VALUES (3, 'George.harrison@beatles.com', 'George', 'Harrison', NULL)
-INSERT INTO [dbo].[Email] ([id], [email], [firstname], [lastname], [address]) VALUES (4, 'ringo.starr@beatles.com', 'Ringo', 'Starr', NULL)
+INSERT INTO [dbo].[Email] ([id], [email], [firstname], [lastname], [address]) VALUES (4, 'ringo.starr@beatles', 'Ringo', 'Starr', NULL)
